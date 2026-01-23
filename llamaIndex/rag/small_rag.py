3c9bea9bb9fe3a06a4cff24978552a3e772b7550
Akash200307 <akash200307@outlook.com>
2026-01-23 22:25:01 +0530

    used streamlit as a beginner on llamaIndex/rag

--- llamaIndex/rag/small_rag.py
+++ llamaIndex/rag/small_rag.py
@@ -3,9 +3,11 @@ from llama_index.llms import groq
 from llama_index.embeddings import huggingface
 from llama_index.core import VectorStoreIndex,SimpleDirectoryReader
 from llama_index.core import Settings
+import streamlit as st
 load_dotenv()
 
-
+st.set_page_config(page_title="RAG with Groq + LlamaIndex",layout='wide')
+st.title("Upload files -> Ask questions")
 llm=groq.Groq(model='moonshotai/kimi-k2-instruct-0905')
 embed=huggingface.HuggingFaceEmbedding(model_name='sentence-transformers/all-MiniLM-L6-v2')
 
@@ -13,9 +15,11 @@ Settings.llm=llm
 Settings.embed_model=embed
 documents=SimpleDirectoryReader('data').load_data()
 index=VectorStoreIndex.from_documents(documents)
-
-
 query_engine=index.as_query_engine()
+user_query=st.text_input("Ask your questions?",placeholder="what is the context?")
 
+if st.button("ask"):
+    response=query_engine.query(user_query)
+    st.write("**ANSWER**")
+    st.write(response.response) # type: ignore
 
-print(query_engine.query("What is the population ?"))
