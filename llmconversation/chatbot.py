7477ed73f4b3158e480bd77a26d5b6f070a7a34e
Akash200307 <akash200307@outlook.com>
2026-02-12 21:17:42 +0530

    testing colab

--- llmconversation/chatbot.py
+++ llmconversation/chatbot.py
@@ -8,8 +8,13 @@ def chatter(message, history):
     res = completion(
         model="groq/moonshotai/kimi-k2-instruct",
         messages=history + [{"role": "user", "content": message}],
+        stream=True
     )
-    return res.choices[0].message.content
+    result =" "
+
+    for chunk in res:
+        result+=chunk.choices[0].delta.content or " "
+        yield result
 
 view = gr.ChatInterface(fn=chatter)
 view.launch()
